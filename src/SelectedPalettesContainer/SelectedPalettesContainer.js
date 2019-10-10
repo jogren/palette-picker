513ea37442b0b5621020e6757a67f66ac9249613
Alan Turing <freemasl.sf@gmail.com>
2019-10-10 15:32:31 -0600

    Setup SelectedPalettesContainer component

---
+++ src/SelectedPalettesContainer/SelectedPalettesContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SelectedPalettesContainer = () => {
+  return (
+    <section>
+      <h2>Project 1</h2>
+      <div>color</div>
+    </section>
+  )
+}
+
+export default SelectedPalettesContainer;
