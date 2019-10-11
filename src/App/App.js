6c71c0a3d68e27d7422cabe39c692e34c590a7e6
Alan Turing <freemasl.sf@gmail.com>
2019-10-11 17:29:50 -0600

    Fix key errors for missing unique keys

--- src/App/App.js
+++ src/App/App.js
@@ -21,7 +21,6 @@ export class App extends Component {
     })
     setCurrentPalette(structuredColors)
     const projects = await getAllProjects();
-    console.log(projects)
     setCurrentProjects(projects)
   }
   
