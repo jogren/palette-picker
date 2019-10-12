94edcd561e034b802d869ffac8a01592fe7f6ea1
Alan Turing <freemasl.sf@gmail.com>
2019-10-12 17:40:54 -0600

    Add action creator test for 'SELECTED_PALETTES' and fix mock data for 'SET_PALETTE'

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -2,32 +2,28 @@ import * as actions from './index';
 
 describe('actions', () => {
   it('should have a type of SET_PALETTE', () => {
-    const mockPalettes = [{
-      id: 5,
-      name: 'palette',
-      project_id: 3,
-      created_at: '2019-10-10T19:13:08.950Z',
-      updated_at: '2019-10-10T19:13:08.950Z',
-      color1: '#C0C0C0',
-      color2: '#3895D3',
-      color3: '#FFFFFF',
-      color4: '#FFFFFF',
-      color5: '#FFFFFF'
-    }];
+    const mockPalettes = [
+      {
+      hexCode: '#cc9639',
+      isLocked: false
+      },
+      {
+        hexCode: '#077c96',
+        isLocked: false
+      }
+    ];
     const expectedAction = {
       type: 'SET_PALETTE',
-      colors: [{
-        id: 5,
-        name: 'palette',
-        project_id: 3,
-        created_at: '2019-10-10T19:13:08.950Z',
-        updated_at: '2019-10-10T19:13:08.950Z',
-        color1: '#C0C0C0',
-        color2: '#3895D3',
-        color3: '#FFFFFF',
-        color4: '#FFFFFF',
-        color5: '#FFFFFF'
-      }]
+      colors: [
+        {
+        hexCode: '#cc9639',
+        isLocked: false
+        },
+        {
+          hexCode: '#077c96',
+          isLocked: false
+        }
+      ]
     }
 
     const result = actions.setCurrentPalette(mockPalettes);
@@ -35,7 +31,7 @@ describe('actions', () => {
     expect(result).toEqual(expectedAction);
   });
 
-  it('should have a type of SETPROJECTS', () => {
+  it('should have a type of SET_PROJECTS', () => {
     const mockProjects = [{
       id: 3,
       name: 'Winter',
@@ -53,6 +49,40 @@ describe('actions', () => {
     }
 
     const result = actions.setCurrentProjects(mockProjects);
+
+    expect(result).toEqual(expectedAction);
+  });
+
+  it('should have a type of SELECTED_PALETTES', () => {
+    const mockPalettes = [{
+      id: 5,
+      name: 'palette',
+      project_id: 3,
+      created_at: '2019-10-10T19:13:08.950Z',
+      updated_at: '2019-10-10T19:13:08.950Z',
+      color1: '#C0C0C0',
+      color2: '#3895D3',
+      color3: '#FFFFFF',
+      color4: '#FFFFFF',
+      color5: '#FFFFFF'
+    }];
+    const expectedAction = {
+      type: 'SELECTED_PALETTES',
+      palettes: [{
+        id: 5,
+        name: 'palette',
+        project_id: 3,
+        created_at: '2019-10-10T19:13:08.950Z',
+        updated_at: '2019-10-10T19:13:08.950Z',
+        color1: '#C0C0C0',
+        color2: '#3895D3',
+        color3: '#FFFFFF',
+        color4: '#FFFFFF',
+        color5: '#FFFFFF'
+      }]
+    }
+
+    const result = actions.setSelectedPalettes(mockPalettes);
 
     expect(result).toEqual(expectedAction);
   });
