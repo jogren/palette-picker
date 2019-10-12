81334349bb505756921242302638712907a974f4
Alan Turing <freemasl.sf@gmail.com>
2019-10-12 15:06:20 -0600

    Create snapshot test for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App } from './App';
+import { setCurrentPalette, setCurrentProjects } from '../actions';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let wrapper;
+  const setCurrentPaletteMock = jest.fn();
+  const setCurrentProjectsMock = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(<App 
+      setCurrentPalette={setCurrentPaletteMock}
+      setCurrentProjects={setCurrentProjectsMock} />)
+  });
+
+  it('should match the snapshot with the data passed through', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 });
