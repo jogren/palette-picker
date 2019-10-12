ba31bfa27d04dbea0c1900c85496ec233c647dfc
Jacob Ogren <jogren0044@gmail.com>
2019-10-12 16:32:12 -0600

    Add lock and unlock icons for colors

--- src/Color/Color.js
+++ src/Color/Color.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { FaLockOpen } from 'react-icons/fa';
+import { FaLock } from 'react-icons/fa';
 
-const Color = ({ color, toggleLock }) => {
+
+const Color = ({ color, isLocked, toggleLock }) => {
+  let lockedImg = isLocked ? <FaLock /> : <FaLockOpen />
+
   return (
     <article className="color" style={color}>
-      <button onClick={() => toggleLock(color.backgroundColor)}>lock</button>
+      <button onClick={() => toggleLock(color.backgroundColor)}>{lockedImg}</button>
       <p>{color.backgroundColor}</p>
     </article>
   )
