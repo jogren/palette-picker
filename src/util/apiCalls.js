0404732582d7580afd46bfcdcb911e3f81d73818
Jacob Ogren <jogren0044@gmail.com>
2019-10-12 18:17:11 -0600

    Add delete project functionality, DOM reflects changes to database immediately

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -78,3 +78,21 @@ export const deletePaletteFromDB = async (id) => {
   }
 }
 
+export const deleteProjectFromDB = async (id) => {
+  const url = `https://palette-picker-api-sfjo.herokuapp.com/api/v1/projects/${id}`;
+  try {
+    const options = {
+      method: "DELETE",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    const response = await fetch(url, options)
+    if (!response.ok) {
+      throw new Error('There was an error deleting that palette');
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
