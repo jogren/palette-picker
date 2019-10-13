967e41e449ca40842f396ab39b6a474952a61f98
Alan Turing <freemasl.sf@gmail.com>
2019-10-12 18:58:09 -0600

    Test currentPalette reducer

---
+++ src/reducers/currentPalette.test.js
@@ -0,0 +1,41 @@
+import { currentPalette } from './currentPalette';
+
+describe('currentPalette reducer', () => {
+  it('should return the initial state', () => {
+    const expected = [];
+
+    const result = currentPalette(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return the new state with all of the current colors when SET_PALETTE action is passed through', () => {
+    const expected = [
+      {
+      hexCode: '#cc9639',
+      isLocked: false
+      },
+      {
+        hexCode: '#077c96',
+        isLocked: false
+      }
+    ]
+    const actionObj = {
+      type: 'SET_PALETTE',
+      colors: [
+        {
+        hexCode: '#cc9639',
+        isLocked: false
+        },
+        {
+          hexCode: '#077c96',
+          isLocked: false
+        }
+      ]
+    }
+
+    const result = currentPalette(undefined, actionObj);
+
+    expect(result).toEqual(expected);
+  })
+});
