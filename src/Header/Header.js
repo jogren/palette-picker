4f17aa1339de9c5766a32257160034548c2153f9
Alan Turing <freemasl.sf@gmail.com>
2019-10-15 13:57:34 -0600

    Setup structure for header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,7 +4,7 @@ const Header = ({ generateColors }) => {
   return (
     <header>
       <h1>Palette Picker</h1>
-      <button onClick={generateColors}>Generate New Palette</button>
+      <button className="generate-color-btn" onClick={generateColors}>Generate New Palette</button>
     </header>
   )
 }
