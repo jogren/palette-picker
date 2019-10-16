2af0eb16cb4fc659d9e79415673e836def7f6bf1
Alan Turing <freemasl.sf@gmail.com>
2019-10-15 21:03:44 -0600

    Mock Implementation of apiCalls

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,7 +1,38 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { App } from './App';
-import { setCurrentPalette, setCurrentProjects } from '../actions';
+import { setCurrentPalette, setCurrentProjects, setSelectedPalettes, setCurrentPaletteId, hasErrored } from '../actions';
+import { getAllProjects, deletePaletteFromDB, getSelectedPalettes } from '../util/apiCalls';
+
+jest.mock('../../util/apiCalls');
+
+getAllProjects.mockImplementation(() => {
+  return Promise.resolve([{
+    id: 25,
+    name: 'Mountains',
+    created_at: '2019-10-15T21:40:24.372Z',
+    updated_at: '2019-10-15T21:40:24.372Z'
+  }])
+});
+
+deletePaletteFromDB.mockImplementation(() => {
+  return Promise.resolve()
+});
+
+getSelectedPalettes.mockImplementation(() => {
+  return Promise.resolve([{
+    id: 49,
+    name: '12345',
+    project_id: 13,
+    created_at: '2019-10-16T00:35:10.929Z',
+    updated_at: '2019-10-16T00:35:10.929Z',
+    color1: '#3890af',
+    color2: '#1aaf38',
+    color3: '#a567c6',
+    color4: '#fcf4a1',
+    color5: '#7a35c4'
+  }])
+});
 
 describe('App', () => {
   let wrapper;
