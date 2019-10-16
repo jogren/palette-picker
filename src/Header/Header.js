1b12631cb49630f955e6f7649374a60e88021ac6
Jacob Ogren <jogren0044@gmail.com>
2019-10-16 14:03:20 -0600

    Update margin and padding for projectContainer and selectedPalettesContainer

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ generateColors }) => {
   return (
-    <header>
+    <header className="Header_header">
       <h1>Palette Picker</h1>
       <button className="generate-color-btn" onClick={generateColors}>Generate New Palette</button>
     </header>
