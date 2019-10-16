1e5410cbe9f3269f7baed3321222765e960302ea
Alan Turing <freemasl.sf@gmail.com>
2019-10-16 12:40:49 -0600

    Test mapStateToProps in CreatePaletteForm

--- src/CreatePaletteForm/CreatePaletteForm.test.js
+++ src/CreatePaletteForm/CreatePaletteForm.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { CreatePaletteForm } from './CreatePaletteForm';
+import { CreatePaletteForm, mapStateToProps, mapDispatchToProps } from './CreatePaletteForm';
 import { postNewPalette, getSelectedPalettes } from '../util/apiCalls';
-import { mapStateToProps, mapDispatchToProps } from './CreatePaletteForm';
 
 jest.mock('../util/apiCalls');
 
@@ -40,6 +39,30 @@ describe('CreatePaletteForm', () => {
     isLocked: false
   }
 ]
+  const selectedPalettesMock = [{
+    id: 49,
+    name: '12345',
+    project_id: 13,
+    created_at: '2019-10-16T00:35:10.929Z',
+    updated_at: '2019-10-16T00:35:10.929Z',
+    color1: '#3890af',
+    color2: '#1aaf38',
+    color3: '#a567c6',
+    color4: '#fcf4a1',
+    color5: '#7a35c4'
+  }]
+  const currentPaletteIdMock = {
+    id: 49,
+    name: '12345',
+    projectId: 13
+  }
+  const mockState = {
+    currentPalette: currentPaletteMock,
+    currentProjects: currentProjectsMock,
+    selectedPalettes: selectedPalettesMock,
+    currentPaletteId: currentPaletteIdMock,
+    errorMsg: ""
+  }
 
   beforeEach(() => {
     wrapper = shallow(<CreatePaletteForm 
@@ -59,19 +82,19 @@ describe('CreatePaletteForm', () => {
   it('should update name state onChange of input', () => {
     wrapper.find('input').at(0).simulate('change', { target: { value: 'New Palette', name: 'name' } });
     expect(wrapper.state('name')).toEqual('New Palette')
-  })
+  });
 
   it('should update currentProject state onChange of input', () => {
     wrapper.find('select').at(0).simulate('change', { target: { value: 'Project 1', name: 'currentProject' } });
     expect(wrapper.state('currentProject')).toEqual('Project 1')
-  })
+  });
 
   it('should call clearSelectedPaletteId and setRandomPalette when reverEdits is called', () => {
     wrapper.instance().revertEdits();
 
     expect(mockClearSelectedPaletteId).toHaveBeenCalled();
     expect(mockSetRandomPalette).toHaveBeenCalled();
-  })
+  });
 
   it('should run all items in the try when handleSubmit is called', async () => {
     let mockEvent = { preventDefault: () => {} };
@@ -83,7 +106,7 @@ describe('CreatePaletteForm', () => {
     await expect(getSelectedPalettes).toHaveBeenCalled();
     expect(mockSetSelectedPalettes).toHaveBeenCalled();
     expect(wrapper.state('name')).toEqual("")
-  })
+  });
 
   it('should run all items in the try when handleSaveEdits is called', async () => {
     wrapper.instance().handleSaveEdits();
@@ -92,12 +115,24 @@ describe('CreatePaletteForm', () => {
     await expect(mockSetSelectedPalettes).toHaveBeenCalled();
     expect(mockClearSelectedPaletteId).toHaveBeenCalled();
     expect(mockSetRandomPalette).toHaveBeenCalled();
-  })
+  });
 
   it.skip('should call handleSaveEdits on button click', () => {
     wrapper.instance().handleSaveEdits = jest.fn();
     // wrapper.instance().forceUpdate();
     wrapper.find('button').first().simulate('click');
     expect(wrapper.instance().handleSaveEdits).toHaveBeenCalled()
-  })
+  });
+
+  it('mapStateToProps should grab the props it needs', () => {
+    const expected = {
+      currentPalette: currentPaletteMock,
+      currentProjects: currentProjectsMock,
+      currentPaletteId: currentPaletteIdMock
+    }
+
+    const mappedProps = mapStateToProps(mockState);
+
+    expect(mappedProps).toEqual(expected);
+  });
 });
