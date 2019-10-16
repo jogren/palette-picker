907c70359a742c944ed8bbfef4fc651f1c8b700e
Jacob Ogren <jogren0044@gmail.com>
2019-10-16 14:50:15 -0600

    Add informative message to click on the color to lock them in place

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Header = ({ generateColors }) => {
+const Header = ({ generateColors, lockMessage }) => {
   return (
     <header className="Header_header">
       <h1>Palette Picker</h1>
+      {lockMessage && <p>Click on a color to lock...</p>}
       <button className="generate-color-btn" onClick={generateColors}>Generate New Palette</button>
     </header>
   )
