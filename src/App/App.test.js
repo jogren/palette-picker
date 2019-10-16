69c32ef491a1b1d64ae676fdb1b6a3bebf1f4355
Alan Turing <freemasl.sf@gmail.com>
2019-10-15 21:12:25 -0600

    Update props to include recent additions for redux

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -4,7 +4,7 @@ import { App } from './App';
 import { setCurrentPalette, setCurrentProjects, setSelectedPalettes, setCurrentPaletteId, hasErrored } from '../actions';
 import { getAllProjects, deletePaletteFromDB, getSelectedPalettes } from '../util/apiCalls';
 
-jest.mock('../../util/apiCalls');
+jest.mock('../util/apiCalls');
 
 getAllProjects.mockImplementation(() => {
   return Promise.resolve([{
@@ -38,16 +38,35 @@ describe('App', () => {
   let wrapper;
   const setCurrentPaletteMock = jest.fn();
   const setCurrentProjectsMock = jest.fn();
+  const setSelectedPalettesMock = jest.fn();
+  const setCurrentPaletteIdMock = jest.fn();
+  const hasErroredMock = jest.fn();
   const currentPaletteMock = [{
     hexCode: "#25b25d",
     isLocked: false
+  }];
+  const selectedPalettesMock = [{
+    id: 49,
+    name: '12345',
+    project_id: 13,
+    created_at: '2019-10-16T00:35:10.929Z',
+    updated_at: '2019-10-16T00:35:10.929Z',
+    color1: '#3890af',
+    color2: '#1aaf38',
+    color3: '#a567c6',
+    color4: '#fcf4a1',
+    color5: '#7a35c4'
   }]
   
   beforeEach(() => {
     wrapper = shallow(<App 
       setCurrentPalette={setCurrentPaletteMock}
       setCurrentProjects={setCurrentProjectsMock}
-      currentPalette={currentPaletteMock} 
+      currentPalette={currentPaletteMock}
+      selectedPalettes={selectedPalettesMock}
+      setSelectePalettes={setSelectedPalettesMock}
+      setCurrentPaletteId={setCurrentPaletteIdMock}
+      hasErrored={hasErroredMock} 
       />)
   });
 
